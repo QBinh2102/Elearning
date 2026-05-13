4681a41856e377d96602080438abc4556476c5b4
QBinh2102 <toquocbinh2102@gmail.com>
2026-05-13 10:25:30 +0700

    Add render.yaml

--- backend/run.py
+++ backend/run.py
@@ -1,12 +1,17 @@
+import os
 from app import create_app
 from app.configs.database_config import db
 from app.configs.socketIO_config import socketio
 from flask_jwt_extended import JWTManager
 
 app = create_app()
+
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
     jwt = JWTManager(app)
-    socketio.run(app, debug=True)
+    
+    port = int(os.environ.get("PORT", 10000))
+    
+    socketio.run(app, host="0.0.0.0", port=port)
